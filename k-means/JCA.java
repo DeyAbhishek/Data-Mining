95e9ca374dbbb883363eb64d7e0ced984dd0c3ea
Abhishek Dey <DeyAbhishek@users.noreply.github.com>
2015-09-16 16:17:23 -0400

    Update JCA.java

--- k-means/JCA.java
+++ k-means/JCA.java
@@ -1,14 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package kmean2;
-
-/**
- *
- * @author VAIO
- */
 
 import java.util.Vector;
 
