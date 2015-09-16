86b60fdcd786b78082035860a377477c2080deef
Abhishek Dey <DeyAbhishek@users.noreply.github.com>
2015-09-16 16:16:53 -0400

    Update Cluster.java

--- k-means/Cluster.java
+++ k-means/Cluster.java
@@ -1,15 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package kmean2;
 
 import java.util.Vector;
-/**
- *
- * @author Abhishek Dey
- */
 
 
 import java.util.Vector;
