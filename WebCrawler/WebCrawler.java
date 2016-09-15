68479a72b334087a7c4f3a00b854aa4853d2edc7
Abhishek Dey <DeyAbhishek@users.noreply.github.com>
2016-09-15 17:29:02 -0500

    Update and rename web-crawler/WebCrawler.java to WebCrawler/WebCrawler.java

---
+++ WebCrawler/WebCrawler.java
@@ -0,0 +1,85 @@
+package WebCrawler;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class WebCrawler {
+
+	private Queue<String> queue;  //type is String since URLs are String
+	private List<String> discoveredWebsiteList;
+	
+	private int depth = 0;  // depth in the network till which we will continue searching
+
+	public WebCrawler() {
+		queue = new LinkedList<>();
+		this.discoveredWebsiteList = new ArrayList<>();
+	}
+	
+	/*
+	 * discoverWeb methods actually crawls the web and discover the websites 
+	 * using BFS strategy
+	 */
+	public void discoverWeb(String root) {
+		
+		//BFS algorithm is implemented below
+		this.queue.add(root);
+		this.discoveredWebsiteList.add(root);
+
+		while (!queue.isEmpty()) {
+
+			String v = this.queue.remove();
+			String rowHtml = readURL(v);  // read the html content of the newly found website
+
+			String regexp = "https://(\\w+\\.)*(\\w+)";  // reg exp for url pattern starting with https://
+			Pattern pattern = Pattern.compile(regexp);
+			Matcher matcher = pattern.matcher(rowHtml);  // search for pattern in the html content
+			
+			while( matcher.find() ){  
+				String w = matcher.group();  //group() -> Returns the input subsequence matched by the previous match.
+				
+				if( !discoveredWebsiteList.contains(w) ){
+					discoveredWebsiteList.add(w);
+					System.out.println("Website found with URL: " + w);
+					queue.add(w);
+				}
+			}
+			
+			depth++;
+			if (depth == 10) {
+				break;  // searching only till depth = 10 when we have to many websites crawled
+			}
+		}
+	}
+	
+	/*
+	 * readURL method returns the HTML content of a given website url
+	 */
+	private String readURL(String v) {
+		
+		String rawHtml = "";
+		
+		try {
+			URL url = new URL(v);  // creating URL out of input string v
+			BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+			//openStream() -> Opens a connection to this URL and returns an InputStream for reading from that connection.
+
+			String inputLine = "";
+			while ((inputLine = in.readLine()) != null) {
+				rawHtml += inputLine;
+			}
+
+			in.close();				
+		} catch (Exception e) {
+			System.out.println("Error reading from " + v);
+		}
+		
+		return rawHtml;
+	}
+}
